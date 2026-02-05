45f19b904dfc622cebcd31bea1d6f54fe050cc9c
Amelia Morris <amelia.morris@ucalgary.ca>
2026-02-04 20:11:26 -0700

    Updated gravity.py file.

--- src/GOPH547Lab01/gravity.py
+++ src/GOPH547Lab01/gravity.py
@@ -1,7 +1,6 @@
 def gravity_potential_point(x, xm, m, G=6.674e-11) :
     """Compute the gravity potential due to a point mass.
-
-
+    
     Parameters
     ----------
     x : array_like, shape=(3,)
@@ -20,6 +19,21 @@ def gravity_potential_point(x, xm, m, G=6.674e-11) :
     float
         Gravity potential at x due to anomaly at xm.
     """
+
+    #Variable Definitions:
+    G = float(6.674e-11)
+    x = np.asarray(x, dtype = float).reshape(3)
+    xm = np.asarray(xm, dtype = float).reshape(3)
+    r_vec = x - xm
+    r = np.linalg.norm(r_vec)
+    #Prevents equation from being divided by zero.
+    if r == 0.0:
+        raise ValueError("Survey point and point mass locations cannot be the same value.")
+
+    #Calculating gravity potential.
+    U = (G * m) / r
+
+    return float(U)
 
 def gravity_effect_point(x, xm, m, G=6.674e-11) :
     """Compute the vertical gravity effect due to a point mass (positive downward).
@@ -42,3 +56,21 @@ def gravity_effect_point(x, xm, m, G=6.674e-11) :
     float
         Gravity effect at x due to anomaly at xm.
     """
+
+    #Variable Definitions:
+    G = float(6.674e-11)
+    x = np.asarray(x, dtype = float).reshape(3)
+    xm = np.asarray(xm, dtype = float).reshape(3)
+    r_vec = x - xm
+    r = np.linalg.norm(r_vec)
+    #Prevents equation from being divided by zero.
+    if r == 0.0:
+        raise ValueError("Survey point and point mass location cannot be the same.")
+        
+    z_diff = x[2] - xm[2]
+
+    #Calculating the vertical gravity effect due to point mass.
+    gz = (G * m * z_diff) / (r * r * r)
+
+    return float(gz)
+
